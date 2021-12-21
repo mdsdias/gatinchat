0163fe0091142b3668a6ddfbb95aed803e8b6746
Isqne <isqne@pm.me>
2021-12-21 00:09:09 +0000

    testing

--- index.js
+++ index.js
@@ -1,77 +1,28 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const ejs = require('ejs');
+
 const port = process.env.PORT || 3000;
 var current;
 
 app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
 
-app.get('/:current', (req, res) => {
-  res.render('index.html');
 
+app.get('/chat/:id', (req, res) => {
+  res.render('index.ejs');
+  current = req.params.id;
 });
-
-// chat p2p
-navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function (stream) {
-    // This is used for Signaling the Peer
-    const signalhub = require('signalhub')
-    const createSwarm = require('webrtc-swarm')
-    // Creates the Signal rub running in the mentioned port
-    const hub = signalhub('my-game', [
-      'http://localhost:8080'
-    ])
-    const swarm = createSwarm(hub, {
-      stream: stream
-    })
-    //Creates a video player
-    const Player = require('./videoplayer.js')
-    const you = new Player({ x: 0, y : 0 ,color : 'black',left : 0,top : 0})
-    you.addStream(stream)
-
-    const players = {}
-    swarm.on('connect', function (peer, id) {
-      if (!players[id]) {
-        players[id] = new Player({
-            x : 300,
-            y : 0,
-            left : 200,
-            top : 0,
-            color : 'red'
-        })
-        peer.on('data', function (data) {
-          data = JSON.parse(data.toString())
-          players[id].update(data)
-        })
-        players[id].addStream(peer.stream)
-      }
-    })
-    //On webRTC Disconnets
-    swarm.on('disconnect', function (peer, id) {
-      if (players[id]) {
-        players[id].element.parentNode.removeChild(players[id].element)
-        delete players[id]
-      }
-    })
-
-
-    setInterval(function () {
-      you.update()
-
-      const youString = JSON.stringify(you)
-      swarm.peers.forEach(function (peer) {
-        peer.send(youString)
-      })
-    }, 100)
-  })
 
 io.on('connection', (socket) => {
   io.emit('started', socket.id)
   socket.on('chat message', (msg) => {
-    if (current !== undefined) {
+    if (current) {
         socket.join(current)
-        io.sockets.emit('chat message', msg)
+        socket.to(current).emit('chat message', msg, socket.id)
     } else {
-        io.emit('chat message', msg);
+        io.emit('chat message', msg, socket.id);
     }
   });
 });
--- libs.js
+++
@@ -1,6 +0,0 @@
-function criarMensagem(msg) {
-  var item = document.createElement('li');
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-}
